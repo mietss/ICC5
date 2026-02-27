cab4f930fedbe96cef19b33e60334d102e11d534
Mies VanBeek <vanbeekmies@gmail.com>
2026-02-27 13:29:59 -0700

    implemented function

--- ICC5.c
+++ ICC5.c
@@ -3,7 +3,19 @@
 //
 #include <stdio.h>
 
-int main(void) {
+int sort_pivot(int* main_arr, int main_size, int pivot, int* array1, int* array2) {
+    int j = 0;
+    int k = 0;
+    for (int i = 0; i < main_size; i++) {
+        if (*(main_arr + i) > pivot) {
+            *(array2 + j) = *(main_arr + i);
+            j++;
+        }
+        else {
+            *(array1 + k) = *(main_arr + i);
+            k++;
+        }
+    }
 
-    return 0;
+    return k + 1;
 }
